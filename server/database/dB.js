3e6abe236ad96927be531fc7c09a4e2e005e493f
YogeshJaiswal7 <mukeshjaiswal9826@gmail.com>
2025-04-12 12:38:38 +0530

    new

--- server/database/dB.js
+++ server/database/dB.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB = async()=>{
-    try {
-       await mongoose.connect(process.env.MONGO_URI)
-       console.log('MongoDb connected') 
-    } catch (error) {
-        console.log(error)
+const connectDB= async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Database connected successfully');
+    }
+    catch(error){
+        console.log("error occured",error);
     }
 }
 
